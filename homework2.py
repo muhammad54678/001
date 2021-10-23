a48be1802b0f6a5b7a8bf7ada4d0cd4be4fd62b3
muhammad54678 <93044720+muhammad54678@users.noreply.github.com>
2021-10-23 07:38:09 -0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,19 @@
+
+my_list = [47, 45, 67, 82, 78, 76, 85, 96, 100, 115, 345, 876, 0.1]
+small = my_list[0]
+for number in range(0,len(my_list)):
+	if my_list[number] < small:
+		small = my_list[number]
+	print(small)
+	
+
+print("done calculating small, now you can sleep")
+print(small)
+print("plese do not try this at home!!!!!")
+
+
+
+
+# IT WORKS!!!!
+# HORRAY!!!!!!
+#please don't try this at home
